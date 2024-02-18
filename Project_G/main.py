0c474f00cb704e7c49a07a75ec95d9d770f180de
german <nextphasesoon@mail.ru>
2024-02-18 22:55:46 +0300

    project structure

---
+++ Project_G/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def index_hello():
+    return {"message": 'Hello, world!'}
+
+
+if __name__ == "main":
+    uvicorn.run("main:app", reload=True)
